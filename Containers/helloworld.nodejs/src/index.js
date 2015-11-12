af6de72747526e70e444fe9c220b296bd29ca8cc
SergeyDz <dzyuban@gmail.com>
2015-11-12 11:56:56 +0200

    Rabbit container added

--- Containers/helloworld.nodejs/src/index.js
+++ Containers/helloworld.nodejs/src/index.js
@@ -1,7 +1,46 @@
 var express = require('express');
 
+var q = 'tasks';
+
+function bail(err) {
+  console.error(err);
+  process.exit(1);
+}
+ 
+// Publisher 
+function publisher(conn) {
+  conn.createChannel(on_open);
+  function on_open(err, ch) {
+    if (err != null) bail(err);
+    ch.assertQueue(q);
+    ch.sendToQueue(q, new Buffer('something to do'));
+  }
+}
+ 
+// Consumer 
+function consumer(conn) {
+  var ok = conn.createChannel(on_open);
+  function on_open(err, ch) {
+    if (err != null) bail(err);
+    ch.assertQueue(q);
+    ch.consume(q, function(msg) {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        ch.ack(msg);
+      }
+    });
+  }
+}
+
+// require('amqplib/callback_api')
+//   .connect('amqp://guest:guest@localhost', function(err, conn) {
+//     if (err != null) bail(err);
+//     consumer(conn);    
+//     publisher(conn); 
+//   });
+
 // Constants
-var PORT = 8085;
+var PORT = 8087;
 
 // App
 var app = express();
--- Containers/helloworld/src/index.js
+++ Containers/helloworld/src/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
 // Constants
-var PORT = 8086;
+var PORT = 8090;
 
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.send('Hello world from docker container 90\n');
 });
 
 app.listen(PORT);
